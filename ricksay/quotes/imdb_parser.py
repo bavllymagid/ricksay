1a7b05234710b6e663a45e9d10f0c371749a2582
Roman Voropaev <voropaev.roma@gmail.com>
2020-04-28 15:25:40 +0300

    Updated quotes

--- ricksay/quotes/imdb_parser.py
+++ ricksay/quotes/imdb_parser.py
@@ -1,17 +1,27 @@
+import os
 from requests_html import HTMLSession
 from collections import defaultdict
 
 session = HTMLSession()
 r = session.get("https://www.imdb.com/title/tt2861424/quotes")
-characters = ("Rick", "Morty", "Summer", "Jerry", "Beth")
+# characters to save quotes
+characters = ("Rick", "Morty")
+# count quotes for each
 counter = defaultdict(int)
 
 qs = r.html.find("div.sodatext p")
+# iterate over quotes
 for q in qs:
+    # who says
     span = q.find("span.character")
     char = span[0].element.text if len(span) else None
     if char and char in characters:
-        with open('{}.{}'.format(char, counter[char]), 'w') as qfile:
+        # save quotes to separate files in folders
+        if not os.path.isdir(char):
+            os.mkdir(char)
+        fname = "{}/{}.{}".format(char, char, counter[char])
+        with open(fname, "w") as qfile:
+            print("writing {}".format(fname))
+            # says what?
             qfile.write(q.text)
             counter[char] += 1
-

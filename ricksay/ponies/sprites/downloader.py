74c4fa01f821bd4ecc3bc4fce67540c5c384b6ca
Roman Voropaev <voropaev.roma@gmail.com>
2020-05-01 00:22:28 +0300

    New sprites, new spirit

---
+++ ricksay/ponies/sprites/downloader.py
@@ -0,0 +1,32 @@
+# This script parses sprites shared from Pocket Morties game
+
+from requests_html import HTMLSession
+import requests
+import re, shutil, os.path
+from urllib.parse import urlparse
+
+def download_file(url, folder):
+    local_filename = os.path.join(folder, url.split('/')[-1])
+    print("saving", local_filename)
+    with requests.get(url, stream=True) as r:
+        with open(local_filename, 'wb') as f:
+            shutil.copyfileobj(r.raw, f)
+
+session = HTMLSession()
+sprites_url = "https://www.spriters-resource.com/mobile/pocketmortys/"
+host = urlparse(sprites_url).netloc
+characters = ('Rick', 'Morty', 'Beth', 'Jerry', 'Summer')
+for char in characters:
+    if not os.path.exists(char):
+        os.mkdir(char)
+
+page = session.get(sprites_url).html
+sprites = page.find("div.iconbody img")
+
+for sprite in sprites:
+    img_link = sprite.attrs['src']
+    char = sprite.attrs['alt'].split()[-1]
+    url = f"http://{host}/{img_link}" 
+    if char in characters:
+        download_file(url, char)
+

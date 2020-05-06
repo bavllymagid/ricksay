6d472e5aac0a0c2be105bc9b4f472856a3957aab
Roman Voropaev <voropaev.roma@gmail.com>
2020-05-06 20:42:06 +0300

    Script comments

--- ricksay/ponies/sprites/downloader.py
+++ ricksay/ponies/sprites/downloader.py
@@ -1,4 +1,5 @@
 # This script parses sprites shared from Pocket Morties game
+# Depends on Spriters Resource markup...
 
 from requests_html import HTMLSession
 import requests
@@ -7,11 +8,12 @@ from urllib.parse import urlparse
 
 def download_file(url, folder):
     local_filename = os.path.join(folder, url.split('/')[-1])
-    print("saving", local_filename)
+    print("Saving", local_filename)
     with requests.get(url, stream=True) as r:
         with open(local_filename, 'wb') as f:
             shutil.copyfileobj(r.raw, f)
 
+print("Started Schwifting")
 session = HTMLSession()
 sprites_url = "https://www.spriters-resource.com/mobile/pocketmortys/"
 host = urlparse(sprites_url).netloc
@@ -20,6 +22,7 @@ for char in characters:
     if not os.path.exists(char):
         os.mkdir(char)
 
+print("Parsing main page for imgs")
 page = session.get(sprites_url).html
 sprites = page.find("div.iconbody img")
 
@@ -30,3 +33,4 @@ for sprite in sprites:
     if char in characters:
         download_file(url, char)
 
+print("Wubba Lubba Dub Dub, Done!")

eac61a451717a4ddec83e7dc6ee42cc790b01835
Roman Voropaev <voropaev.roma@gmail.com>
2020-05-06 20:32:40 +0300

    New and shiny ponies everywhere!

--- ricksay/ponies/generate.sh
+++ ricksay/ponies/generate.sh
@@ -1,21 +1,24 @@
-#!/bin/bash
+#!/bin/zsh
 # This script generates ponyfiles from downloaded images
+# Prerequisites: Java, Zsh
 
 function export_images {
   # params: character magic_magnify_factor
-  for if in png/$1/*.png; do
+  for if in sprites/$1/*.png; do
     fname=$(basename $if)
     of=$1/${fname%.*}.pony
     if [ ! -f "$of" ]; then
-      echo "Processing $if"
+      print "Processing $if"
       java -jar util-say.jar --import image --magnified $2 --file $if \
         --export ponysay --file "$of"
     fi
   done
 }
 
-# Morties
-export_images Morty 8
-
-# Ricks
-export_images Rick 12
+print "Started Schwifting!"
+for char in Rick Morty Beth Jerry Summer; do
+    print "Time of $char"
+    mkdir -p $char
+    export_images $char 3
+done
+print "Wubba Lubba Dub Dub, Done!"
